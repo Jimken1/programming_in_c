7b5de116505d0084001f3344d0ce6c0dd9dbb707
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-08-31 18:31:27 +0100

    code to create a 7 by 7 square

---
+++ problem_sets/squaree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main (void)
+{
+	void print_square (int height);
+
+	print_square (7);
+;
+}
+
+void print_square (int height)
+{
+	int i = 1;
+	
+	while (i <= height){
+	
+	i++;
+	
+		int j = 1;
+		
+		while (j <= height){
+		
+		j++;
+		
+		printf("*");
+		
+		}
+	printf("\n");
+	}
+
+
+
+
+}
